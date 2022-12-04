7f7bf08154e5247fd5fec7fc774e7e48279e691f
mohlingzheng <mohlingzheng@gmail.com>
2022-12-04 14:33:46 +0800

    delete button, default update, layout change

--- Assignment1/Assignment1/Pages/Record.xaml.cs
+++ Assignment1/Assignment1/Pages/Record.xaml.cs
@@ -18,15 +18,6 @@ namespace Assignment1.Pages
             InitializeComponent();
         }
 
-        public async void Button_Clicked(object sender, EventArgs args)
-        {
-            //statusMessage.Text = "";
-            //App.GameRepo.AddNewGame(newName.Text, newType.Text, newYear.Text, newVersion.Text);
-            //statusMessage.Text = App.GameRepo.StatusMessage;
-            //string result = await DisplayPromptAsync("Question 1", "What's your name?");
-            await PopupNavigation.Instance.PushAsync(new FormPage());
-        }
-
         public async void OnGetButtonClicked(object sender, EventArgs args)
         {
             //statusMessage.Text = "";
